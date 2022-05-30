d92aa242ee06761bc658702d3a99b76a794bea31
ricmapu <ricardo.mayor@gmail.com>
2022-05-30 18:55:53 +0100

    change master data to dataclass Load mct in Cirium file format

--- fl_conn_builder/masterData.py
+++ fl_conn_builder/masterData.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 import csv
 from datetime import timedelta
+from dataclasses import dataclass
+import pandas as pd
 
 
+@dataclass
 class airport:
-    def __init__(self, iatacode="", country=""):
-        self.iatacode = iatacode
-        self.country = country
+    iatacode: str
+    country: str
 
 
+@dataclass
 class mct:
-    def __init__(self, airport, cia_arrival, cia_departure,
-                 type_arrival, type_departure, mct):
-        self.airport = airport
-        self.cia_arrival = cia_arrival
-        self.cia_departure = cia_departure
-        self.type_arrival = type_arrival
-        self.type_departure = type_departure
-        self.mct = mct
+    airport: str
+    cia_arrival: str
+    cia_departure: str
+    type_arrival: str
+    type_departure: str
+    mct: timedelta
 
 
 def load_airports(filename):
@@ -52,11 +53,32 @@ def load_mct(filename):
             cia_departure = data[2]
             type_arrival = data[3]
             type_departure = data[4]
-            horas = int(data[5][0:2])
-            minutos = int(data[5][3:5])
-            maxconnect_time = timedelta(minutes=horas * 60 + minutos)
+            hours = int(data[5][0:2])
+            minutes = int(data[5][3:5])
+            maxconnect_time = timedelta(minutes=hours * 60 + minutes)
 
             mct_dict.append(mct(airport, cia_arrival, cia_departure,
                                 type_arrival, type_departure, maxconnect_time))
+
+    return mct_dict
+
+
+def load_mct_cirium(filename):
+    mct_dict = list()
+
+    xls_file = pd.read_excel(filename, sheet_name='OAG', skiprows=1, dtype=str)
+
+    for index, row in xls_file.iterrows():
+        airport = row.values[1]
+        cia_arrival = row.values[5]
+        cia_departure = row.values[10]
+        type_arrival = row.values[3][0:1]
+        type_departure = row.values[3][1:2]
+        hours = int(row.values[4][0:2])
+        minutes = int(row.values[4][2:4])
+        maxconnect_time = timedelta(minutes=hours * 60 + minutes)
+
+        mct_dict.append(mct(airport, cia_arrival, cia_departure,
+                            type_arrival, type_departure, maxconnect_time))
 
     return mct_dict
